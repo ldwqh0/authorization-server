5cd8bafcba45a3badd0134511d6752cc53f313d8
LiDong <ldwqh0@outlook.com>
2021-05-14 09:45:23 +0800

    修改示例代码

--- authorization-server-core/src/main/java/org/xyyh/authorization/endpoint/UserInfoEndpoint.java
+++ authorization-server-core/src/main/java/org/xyyh/authorization/endpoint/UserInfoEndpoint.java
@@ -1,9 +1,16 @@
 package org.xyyh.authorization.endpoint;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping
+@RequestMapping("userinfo")
 @RestController
 public class UserInfoEndpoint {
+
+    @GetMapping
+    public void getUserInfo() {
+
+    }
+
 }
